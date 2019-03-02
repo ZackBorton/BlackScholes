d11545bf9b38b539bf2bf2088bcde3c9ac846e43
Zack Borton <zaborton@gmail.com>
2019-03-01 21:22:53 -0600

    Cleanup and removing unnecessary  file

--- Logic/Strategy/PutOption.cs
+++ Logic/Strategy/PutOption.cs
@@ -4,6 +4,9 @@ using Models;
 
 namespace Logic
 {
+    /// <summary>
+    ///     European put option, can only be exercised on a specific date, compared to an American call option that can be exercised anytime
+    /// </summary>
     public class PutOption : IOptionStrategy
     {
         /// <summary>
